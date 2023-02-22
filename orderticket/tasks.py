d837fcfad0feed9bf0245d8bad31aa991b40efac
smanojpravin <72971917+smanojpravin@users.noreply.github.com>
2023-02-22 08:28:38 +0530

    Update tasks.py

--- orderticket/tasks.py
+++ orderticket/tasks.py
@@ -1,4 +1,4 @@
-from operator import le
+Okfrom operator import le
 from time import sleep
 from xml.etree.ElementPath import find
 from celery import shared_task
@@ -148,7 +148,7 @@ def equity(connection_check):
 
             # excluding section symbols
             section_check_time = datetime.combine(datetime.now(timezone('Asia/Kolkata')), time(10,15))
-            #LiveHighLow.objects.filter(date__lte=section_check_time.date()).delete()
+            LiveHighLow.objects.filter(date__lte=section_check_time.date()).delete()
             if nowTime > section_check_time.time():
                 # LiveHighLow.objects.all().delete()
                 
